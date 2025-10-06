83a56be5fa8a618d54ad70dd20e1f4f82ecc3a43
Dojo <oscarchanon51715@gmail.com>
2025-10-06 09:29:09 +0000

    update game.cpp

--- MOCK/game.cpp
+++ MOCK/game.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main(){
-    const char* games[] = {"Grow a garden", "Steal a brainrot", "A dusty trip"};
+    const char* games[] = {"Grow A Garden", "Steal A Brainrot", "A Dusty Trip"};
+    
     for(auto s : games){
         cout << s[3] << endl;
     }
+
+    cout << *(*(games+1)+1) << endl;
+
     return 0;
 }
